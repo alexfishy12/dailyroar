e13d489386e5ad0366e8845a03eebeefb542d4b9
alexfishy12 <58009009+alexfishy12@users.noreply.github.com>
2023-04-06 23:40:25 -0400

    Gave website blue background

--- index.php
+++ index.php
@@ -19,10 +19,10 @@ elseif(isset($_SESSION['account_type']) && $_SESSION['account_type']=="GA"){
 <link href="CSS/font_family.css" rel="stylesheet">
     <link href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css" rel="stylesheet" />
 	<link href="CSS/font_family.css" rel="stylesheet">
-  <link href="CSS/home.css" rel="stylesheet" type="text/css"/>
+  <link href="CSS/background.css" rel="stylesheet" type="text/css"/>
 </head>
 
-<body class='retro'>
+<body class='retro' background-image="assets/Background.png"  background-size="cover">
   <div class="wrapper fadeInDown" style="">
     <div id="formContent">
       <!-- Tabs Titles -->
