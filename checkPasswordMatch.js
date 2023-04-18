7c0bbf4b5fa3f3891f4ec6789c68cd0320503bfd
Pankati Patel <pankatipatel08@gmail.com>
2023-04-18 17:27:22 -0400

    update passwordcheck

--- checkPasswordMatch.js
+++ checkPasswordMatch.js
@@ -1,8 +1,12 @@
 // Add change event handler to the input elements
+/*
 $('#password1, #password2').on('change', function() {
     // Extract the values of the input elements
     var string1 = $('#password1').val();
     var string2 = $('#password2').val();
+
+    console.log(string1);
+    console.log(string2);
   
     // Compare the extracted strings
     if (string1 === string2) {
@@ -10,4 +14,15 @@ $('#password1, #password2').on('change', function() {
     } else {
       console.log('Strings do not match.');
     }
+  });
+
+
+*/
+
+  $(document).ready(function() {
+    // Get the text from the input field when its value changes
+    $('.password1').on('change', function() {
+      var inputValue = this.value;
+      console.log('Input value:', inputValue);
+    });
   });
