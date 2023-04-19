be04f461dd183459d2b0a3e50bdc42bca1da52d5
TheGloFather <101736983+TheGloFather@users.noreply.github.com>
2023-04-19 15:24:19 -0400

    Current Password Reset

---
+++ forgot password/newpassword.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+<title>Reset Password</title>
+  <script src= "libraries/papaparse.min.js" ></script>
+  <script type="text/javascript" src="libraries/jquery-3.6.0.min.js"></script>
+  <script src="uploadCSV/uploadCSV.js"></script>
+  <script src="checkPasswordMatch,js"></script>
+  <link href="CSS/font_family.css" rel="stylesheet">
+  <link href="CSS/faculty_home_page.css" rel="stylesheet">
+    <link href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css" rel="stylesheet" />
+</head>
+<body class="retro" style="margin-top:10vh; margin-bottom:30vh; margin-left:20vw; margin-right:20vw;">
+    
+    <div class="body" style="text-align: center">
+        <p>Welcome to the Daily Roar!</p>
+    </div>
+  
+
+<div class="nes-container with-title is-centered">
+  <p class="title">Reset Password</p>
+
+    <?php
+    include "dbconfig.php";
+        if (isset($_POST['code'])){
+            
+            $enteredcode=$_POST['code'];
+            $sql = "SELECT * FROM csemaildb.PasswordCode WHERE Code = '$enteredcode' ";
+            $result = mysqli_query($con, $sql);
+            $count = mysqli_num_rows($result);
+
+            //sql to get database time
+            $sqltime = "SELECT DateOfCode, CURRENT_TIMESTAMP() as current FROM csemaildb.PasswordCode WHERE Code = '$enteredcode' ";
+            $resulttime = mysqli_query($con, $sqltime);
+            $row = mysqli_fetch_assoc($resulttime);
+            
+           
+
+            
+            
+            
+            
+        
+
+            if ($count > 0) {
+
+                if ($diff->i <= 10) {
+
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    $ID = $row["ID"];
+                    $Email = $row["Email"];
+                    $Code = $row["Code"];
+                    $DateOfCode = $row["DateOfCode"]; 
+
+                    echo "<form action='setnewpassword.php' method='post' required='required'>";
+                    echo "<br>Enter your new password: <input type='text' name='password1' required>";
+                    echo "<br>Confirm your password: <input type='text' name='password2' required>";
+                    echo "<input type='hidden' name='Email' value='$Email'>\n";
+                    echo "<input type='submit' value='Submit' id = 'submit-button'>";
+                    echo "</form>";
+
+                    echo "<br>" .$ID."";
+                    echo "<br>" .$Email."";
+                    echo "<br>" .$Code."";
+                    echo "<br>" .$DateOfCode."";
+                    echo "<br>";
+                }
+
+                else {
+                    echo "<br> This code has expired. Please get a new code.";
+                }
+            }
+
+            if ($count == 0) {
+                echo "This code is not in the database";
+            }
+    }
+
+    
+    
+
+    
+    ?>
+  
+  
+
+</div>
+
+
+
+</body>
+</html>
