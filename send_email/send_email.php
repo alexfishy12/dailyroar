71913c40909b6fbae41cd0c2b1814e0b4ec0ed1d
Pankati Patel <pankatipatel08@gmail.com>
2023-04-23 14:30:00 -0400

    multiple attachment

--- send_email/send_email.php
+++ send_email/send_email.php
@@ -222,12 +222,7 @@
         . "Reply-To: sender@example.com\r\n"
         . "Content-Type: multipart/mixed; boundary=boundary1\r\n";
 
-        // Define the email body with attachment
-        $body = "--boundary1\r\n"
-        . "Content-Type: text/html;  charset=UTF-8\r\n"
-        . "Content-Transfer-Encoding: 7bit\r\n\r\n"
-        . "$body\r\n\r\n";
-      
+
         
         // Define the attachment file path and name if file array is not null
         if($attachments != 'NULL'){
@@ -239,7 +234,12 @@
                 // Read the attachment file contents and base64 encode it
                 $attachment_data = chunk_split(base64_encode(file_get_contents($attachment_path)));
 
-
+                                // Define the email body with attachment
+                $body = "--boundary1\r\n"
+                . "Content-Type: text/html;  charset=UTF-8\r\n"
+                . "Content-Transfer-Encoding: 7bit\r\n\r\n"
+                . "$body\r\n\r\n";
+            
                 $body .="--boundary1\r\n"
                 . "Content-Type: application/pdf; name=\"$attachment_name\"\r\n"
                 . "Content-Transfer-Encoding: base64\r\n"
@@ -248,8 +248,9 @@
                
                 }
 
-            $body .= "--boundary1--";
+           
             }
+        $body .= "--boundary1--";
 
         //set baseURL for tracking link
         $base_url = "http://obi.kean.edu/~fisheral/dailyroar/";
