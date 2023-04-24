90b414d5a6328f123a2f17d1e944044f7ef1ddde
alexfishy12 <58009009+alexfishy12@users.noreply.github.com>
2023-04-24 12:39:13 -0400

    Updated faculty_nav and ga_nav to not include uploadcsv.js
    Also changed eamils to send to Students table instead of TestStudents

--- send_email/send_email.php
+++ send_email/send_email.php
@@ -89,7 +89,7 @@
         $values_to_search = [];
 
         // start query
-        $query = "SELECT ID, FirstName, LastName, EmailAddress from csemaildb.TestStudents where ";
+        $query = "SELECT ID, FirstName, LastName, EmailAddress from csemaildb.Students where ";
 
         //paramCount
         $count = 0;
